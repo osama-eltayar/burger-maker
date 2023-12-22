0aa58111251de4dc05dcd08030aa88a265f2823b
osama eltayar <osama.eltayar@4sale.tech>
2023-12-22 20:56:57 +0200

    refactore orderService

--- app/Services/Orders/StoreOrderService.php
+++ app/Services/Orders/StoreOrderService.php
@@ -14,19 +14,24 @@ class StoreOrderService
         return DB::transaction(function () use ($orderData) {
             $order = Order::create([]);
 
-
-            $order->products()->attach(
-                collect($orderData->get('products'))
-                    ->mapWithKeys(
-                        fn($product) => [$product['product_id'] => ['quantity' => $product['quantity']]]
-                    )
-            );
-
-            $order->load('products');
+            $order = $this->attachProductsToOrder($order,$orderData);
 
             event(new OrderCreatedEvent($order));
 
             return $order ;
         });
     }
+
+    private function attachProductsToOrder(Order $order, Collection $orderData)
+    {
+        $order->products()->attach(
+            collect($orderData->get('products'))
+                ->mapWithKeys(
+                    fn($product) => [$product['product_id'] => ['quantity' => $product['quantity']]]
+                )
+        );
+
+        return $order->load('products');
+    }
+
 }
